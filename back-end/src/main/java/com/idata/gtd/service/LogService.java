2a3f30f40d6156174423d702941ee9986c6c8aa7
wincheer <wincheer@126.com>
2018-01-14 17:50:39 +0800

    系统日志的表结构调整以及接口设计

---
+++ back-end/src/main/java/com/idata/gtd/service/LogService.java
@@ -0,0 +1,45 @@
+package com.idata.gtd.service;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.idata.gtd.dao.LogMapper;
+import com.idata.gtd.entity.Log;
+
+/**
+ * <b>版权信息 :</b> 2018，广州智数信息科技有限公司<br/>
+ * <b>功能描述 :</b> 登录restful接口<br/>
+ * <b>版本历史 :</b> <br/>
+ * 杨文清 | 2018年1月13日 上午11:46:27 | 创建
+ */
+@Service
+@Transactional
+public class LogService {
+
+	private final static Logger logger = LoggerFactory.getLogger(LogService.class);
+	@Autowired
+	private LogMapper logDao;
+
+	public int insertLog(Log log) {
+		
+		logger.info(log.getAction()+" - "+log.getTarget());
+		return logDao.insertLog(log);
+	}
+	
+	public int insertLog(Integer actor,String action,String target) {
+		
+		Log log = new Log();
+		log.setActor(actor);
+		log.setOpTime(new Date());
+		log.setAction(action);
+		log.setTarget(target); 
+		
+		return logDao.insertLog(log);
+	}
+
+}
