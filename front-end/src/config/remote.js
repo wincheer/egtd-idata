d887dcfd01c5625b66b1a9e7b8e350187c9fc29a
wincheer <wincheer@126.com>
2018-03-08 14:49:51 +0800

    配置发布环境

--- front-end/src/config/remote.js
+++ front-end/src/config/remote.js
@@ -1,6 +1,6 @@
 //export const base = 'http://192.168.1.232:8080';
 let base = 'http://localhost:8080';
-//let base = 'http://119.29.221.117:8080'; //公司测试服务器
+// let base = 'http://119.29.221.117:8080'; //公司测试服务器
 // let base = 'http://59.41.215.178:8000'; //黄埔公安服务器
 
 export default base;
